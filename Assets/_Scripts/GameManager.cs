533421ebe299a175ef8a8062e9edd1be54d457ff
Coop0 <coop2617@gmail.com>
2024-08-17 11:33:25 +1000

    Basic loading infra

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,13 +1,37 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> _stars = new List<GameObject>();
-    [SerializeField] private List<GameObject> _rocks = new List<GameObject>();
+    [SerializeField] private List<Star> _stars = new List<Star>();
+    [SerializeField] private List<Rock> _rocks = new List<Rock>();
 
     [SerializeField] private PlayerController player;
 
+    public void LoadLevel(GameObject level)
+    {
+        GetStarsForLevel(level);
+        GetRocksForLevel(level);
+    }
 
+    private void GetStarsForLevel(GameObject level)
+    {
+        // Get all Star components in the children of the level GameObject
+        Star[] starsArray = level.GetComponentsInChildren<Star>(true);
+
+        // Clear the existing list and add the new items
+        _stars.Clear();
+        _stars.AddRange(starsArray);
+    }
+
+    private void GetRocksForLevel(GameObject level)
+    {
+        // Get all Rock components in the children of the level GameObject
+        Rock[] rocksArray = level.GetComponentsInChildren<Rock>(true);
+
+        // Clear the existing list and add the new items
+        _rocks.Clear();
+        _rocks.AddRange(rocksArray);
+    }
 }
---
+++ Assets/_Scripts/LevelManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private GameManager gameManager;
+    private GameObject currentLevel;  // Reference to the currently active levelvoid Start()
+
+    public void LoadLevel(string levelName) {
+        // Find the level GameObject by name
+        GameObject levelToLoad = GameObject.Find(levelName);
+
+        if (levelToLoad != null)
+        {
+            // Disable the current level if there is oneif (currentLevel != null)
+            {
+                currentLevel.SetActive(false);
+            }
+
+            // Enable the new level
+            levelToLoad.SetActive(true);
+            currentLevel = levelToLoad;
+            gameManager.LoadLevel(levelToLoad);
+        }
+        else
+        {
+            Debug.LogWarning("Level not found: " + levelName);
+        }
+    }
+
+    public void ReloadCurrentLevel() {
+        if (currentLevel != null)
+        {
+            currentLevel.SetActive(false);
+            currentLevel.SetActive(true);
+        }
+        else
+        {
+            Debug.LogWarning("No current level to reload.");
+        }
+    }
+}
