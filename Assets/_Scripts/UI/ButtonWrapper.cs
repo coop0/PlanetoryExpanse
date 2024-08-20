a19deef66eca569ce1f891a6b318429c5b39a4ec
Jespah Cropley <bluefire3030@gmail.com>
2024-08-20 18:20:44 +1000

    Updated UI

--- Assets/_Scripts/UI/ButtonWrapper.cs
+++ Assets/_Scripts/UI/ButtonWrapper.cs
@@ -1,14 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ButtonWrapper : MonoBehaviour,IPointerDownHandler,IPointerUpHandler
 {
+    public UnityEvent RisingClick;
     [SerializeField] private Image _imgComponent;
     [SerializeField] private Sprite _default, _pressed;
+
     public void OnPointerDown(PointerEventData eventData)
     {
         _imgComponent.sprite = _pressed;
@@ -18,5 +23,6 @@ public class ButtonWrapper : MonoBehaviour,IPointerDownHandler,IPointerUpHandler
     {
         _imgComponent.sprite = _default;
         SoundManager.Instance.PlayRandomHarp();
+        RisingClick?.Invoke();
     }
 }
--- Assets/_Scripts/UI/MainMenu.cs
+++ Assets/_Scripts/UI/MainMenu.cs
@@ -1,17 +1,31 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void LoadCampaign()
-    {
-        // Load the level selection scene
-        SceneManager.LoadScene("LevelSelection");
-    }
+    private WaitForSeconds _harpDelay = new WaitForSeconds(1.5f);
 
     public void ExitGame()
     {
         // Exit the application
         Application.Quit();
+    }
+
+    public void LoadLevel(string levelName)
+    {
+        StartCoroutine(DelayedLoad(levelName));
+    }
+
+    private IEnumerator DelayedLoad(string levelName)
+    {
+        // Wait for harp
+        yield return _harpDelay;
+
+        // Grab level name from serialized prefab
+        PlayerPrefs.SetString("SelectedLevel", "Level" + levelName);
+
+        // Load the main gameplay scene
+        SceneManager.LoadScene("MainGameplay");
     }
 }
